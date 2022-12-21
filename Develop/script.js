76577d53f263317c769bd3fd500725cece993b71
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2022-12-21 07:32:15 -0500

    mapped out todos

--- Develop/script.js
+++ Develop/script.js
@@ -14,6 +14,9 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  //if statement that grabs current time and changes class for color based on whether the time is in the past, the curent hour, or a future hour
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
--- README.md
+++ README.md
@@ -8,6 +8,14 @@ Create a simple calendar application that allows a user to save events for each 
 
 You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
 
+## TO DO
+
+display current date at the top of the calendar
+
+add time blocks for 9am - 5pm
+timeblock colors change based on time of day
+add event listener to save event to local storage
+
 ## User Story
 
 ```md
