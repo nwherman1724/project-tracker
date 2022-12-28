f6f4dbbcf81cf11a128550ec08281defc084c699
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2022-12-28 08:38:40 -0500

    added comment to js and removed code that was not being used

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,17 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
 
-//
-// TODO: Add code to display the current date in the header of the page.
+//used day.js library to pull the date/time and format it to be printed to the page
   var today = dayjs();
   $('#currentDay').text(today.format('MMM D, YYYY'));
 
-    // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-  let timeSlot = document.querySelectorAll('.time-block');
-  let timeSlotArr = [timeSlot];
+  //sets currentTime equal to the time in hours
   let currentTime = dayjs().format('H');
-  console.log(currentTime)
-  console.log(timeSlotArr)
 
+
+  // Function that changes the color of the time block based on the time time of day
   function changeColor() {
+
+  // initializing variable that point to each time block element
   var nine = document.getElementById('hour-9');
   var ten = document.getElementById('hour-10');
   var eleven = document.getElementById('hour-11');
@@ -30,15 +21,20 @@ $(function () {
   var fifteen = document.getElementById('hour-15');
   var sixteen = document.getElementById('hour-16');
   var seventeen = document.getElementById('hour-17');
-  console.log(nine);
 
+  // if currentTime is greater than 9 remove the class present and add the class past
+  // this changes the color
   if(currentTime > 9) {
     nine.classList.remove('present')
     nine.classList.add('past')
-  } else if (currentTime < 9){
+  } 
+  // if currentTime is less than 9 remove the class present and add the class future
+  // this changes the color
+  else if (currentTime < 9){
     nine.classList.remove('present')
     nine.classList.add('future')
   }
+  // same logic in lines 39-37 applies in lines 50-105
   if(currentTime > 10) {
     ten.classList.remove('present')
     ten.classList.add('past')
@@ -96,16 +92,10 @@ $(function () {
     seventeen.classList.add('future')
   }
   }
+
+  // Calling change color function
   changeColor();
   
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
 
   let save_9 = document.getElementById('save-9');
   let timeBlock_9 = document.querySelector('#text-9')
@@ -293,11 +283,6 @@ $(function () {
   //gets text-11 from local storage upon page load.
   let input_17 = localStorage.getItem('text-17');
   timeBlock_17.textContent = input_17;
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
  
 
 });
